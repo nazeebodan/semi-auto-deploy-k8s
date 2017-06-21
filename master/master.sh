533835dd77a19fc61920ccfa2f0317a07594e25d
suredandan <xrzp@qq.com>
2017-06-21 16:53:22 +0800

    some modify

--- master/master.sh
+++ master/master.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 #######Begin########
-echo "It will install k8s-master."
+echo "=====================>begin to install k8s-master<======================"
 sleep 1
 
 ##check last command is OK or not.
@@ -26,15 +26,29 @@ fi
 
 ##some env
 
-hostname=`hostname`
 baseDir="/softdb"
-file="kubernetes-server-linux-amd64.tar.gz"
+k8s_file="kubernetes-server-linux-amd64.tar.gz"
+etcd_version="v3.1.9"
+etcd_file="etcd/etcd-v3.1.9-linux-amd64.tar.gz"
+flannel_version="v0.7.1"
+flannel_file="flannel-v0.7.1-linux-amd64.tar.gz"
 serviceDir="/lib/systemd/system"
+MASTER_NAME=`hostname`
+MASTER_IP=`ifconfig eth0|sed -n '2p'|awk '{print $2}'|cut -c 1-20`
+ETC_NAME=etcd-`hostname`
+mkdir -p /var/lib/etcd
 mkdir -p /etc/kubernetes/
-rm -rf /etc/kubernetes/*
 rm -rf /lib/systemd/system/kube*
 
-function closeSelinux(){
+backService(){
+    if [  -f "${serviceDir}/$1" ]; then
+		echo "step:------>backup service config file "$1
+		sleep 1
+		mv ${serviceDir}/$1 ${serviceDir}/$1.bak
+    fi
+}
+
+closeSelinux(){
     echo "step:------> close selinux config"
 	sleep 1
     sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
@@ -48,15 +62,9 @@ function closeSelinux(){
 	sleep 1
 }
 
-function backService(){
-        if [  -f "$serviceDir/$1" ]; then
-		echo "step:------>backup service config file "$1
-        	sleep 1
-		mv $serviceDir/$1 $serviceDir/$1.bak
-        fi
-}
 
-function closeIptables(){
+
+closeIptables(){
 	echo "step:------> stop iptables "
 	sleep 1
         iptables-save > /etc/sysconfig/iptables_`date +%s`
@@ -66,7 +74,7 @@ function closeIptables(){
 	sleep 1
 }
 
-function enableAndStarService(){
+enableAndStarService(){
 	echo "step:------> startup $1 "
 	sleep 1
 	systemctl daemon-reload
@@ -77,14 +85,14 @@ function enableAndStarService(){
 	sleep 1
 }
 
-function createK8scomponents(){
-        if [ ! -f "$file" ]; then
-                echo "$file is not exist!"
-                exit 0
-        fi
+createK8scomponents(){
+    if [ ! -f "$baseDir/master/${k8s_file}" ]; then
+        echo "${k8s_file} is not exist!"
+        exit 0
+    fi
 	echo "step:------> unzip k8s-package"
 	sleep 1
-    tar -zxf $file
+    tar -zxf $k8s_file
     cd kubernetes
 	echo "step:------> unzip k8s-package comleted."
 	sleep 1
@@ -94,73 +102,141 @@ function createK8scomponents(){
     cp -r server/bin/{kube-apiserver,kube-controller-manager,kube-scheduler,kubectl} /usr/bin/
     chmod 755 /usr/bin/kube*
 	check_ok
-	echo "step:------> copy kube-master components to /usr/bin completed."
+	echo "step:------> copy kube-node components to /usr/bin completed."
 	sleep 1
 	rm -rf kubernetes
 }
 
-function createConfigFiles(){
-	echo "step:------> create k8s config "
-	sleep 1
-	sed -e "s/masterhostname/$hostname/g" $baseDir/master/repo/config.repo > $baseDir/master/repo/config.$hostname
-	check_ok
-	mv $baseDir/master/repo/config.$hostname /etc/kubernetes/config
-	echo "step:------> create k8s config comleted."
-	sleep 1
+createConfigFiles(){
 	
-	echo "step:------> create apiserver config "
-	sleep 1
-	sed -e "s/masterhostname/$hostname/g" $baseDir/master/repo/apiserver.repo > $baseDir/master/repo/apiserver.$hostname
-	check_ok
-	mv $baseDir/master/repo/apiserver.$hostname /etc/kubernetes/apiserver
-	echo "step:------> create apiserver config comleted."
-	sleep 1
-	
-	echo "step:------> create scheduler config "
-	sleep 1
-	cp $baseDir/master/repo/scheduler.repo /etc/kubernetes/scheduler
-	echo "step:------> create scheduler config comleted."
-	sleep 1
-	
-	echo "step:------> create controller-manager config "
-	sleep 1
-	cp $baseDir/master/repo/controller-manager.repo /etc/kubernetes/
-	mv /etc/kubernetes/controller-manager.repo /etc/kubernetes/controller-manager
-	echo "step:------> create controller-manager config comleted."
-	sleep 1
-	
+	#创建 apiserver.service
+	cat > kube-apiserver.service <<EOF
+[Unit]
+Description=Kubernetes API Server
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+After=network.target
+After=etcd.service
+[Service]
+ExecStart=/usr/bin/kube-apiserver \\
+--admission-control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ServiceAccount,DefaultStorageClass,ResourceQuota \\
+--advertise-address=${MASTER_NAME} \\
+--bind-address=${MASTER_NAME} \\
+--insecure-bind-address=${MASTER_NAME} \\
+--authorization-mode=RBAC \\
+--runtime-config=rbac.authorization.k8s.io/v1alpha1 \\
+--kubelet-https=true \\
+--experimental-bootstrap-token-auth \\
+--token-auth-file=/etc/kubernetes/token.csv \\
+--service-cluster-ip-range=10.254.0.0/16 \\
+--service-node-port-range=30000-32767 \\
+--tls-cert-file=/etc/kubernetes/ssl/kubernetes.pem \\
+--tls-private-key-file=/etc/kubernetes/ssl/kubernetes-key.pem\\
+--client-ca-file=/etc/kubernetes/ssl/ca.pem \\
+--service-account-key-file=/etc/kubernetes/ssl/ca-key.pem \\
+--etcd-cafile=/etc/kubernetes/ssl/ca.pem \\
+--etcd-certfile=/etc/kubernetes/ssl/kubernetes.pem \\
+--etcd-keyfile=/etc/kubernetes/ssl/kubernetes-key.pem \\
+--enable-swagger-ui=true \\
+--apiserver-count=3 \\
+--audit-log-maxage=30 \\
+--audit-log-maxbackup=3 \\
+--audit-log-maxsize=100 \\
+--audit-log-path=/var/lib/audit.log \\
+--event-ttl=1h \\
+--etcd-servers=http://${MASTER_NAME}:2379 \\
+--logtostderr=true \\
+--v=0 \\
+--allow-privileged=true
+--master=http://${MASTER_NAME}:8080
+Restart=on-failure
+Type=notify
+LimitNOFILE=65536
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+	#创建controller.servic
+	cat > kube-controller-manager.service <<EOF
+[Unit]
+Description=Kubernetes Controller Manager
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+[Service]
+ExecStart=/usr/bin/kube-controller-manager \\
+--address=127.0.0.1 \\
+--allocate-node-cidrs=true \\
+--service-cluster-ip-range=10.254.0.0/16 \\
+--cluster-name=kubernetes \\
+--cluster-signing-cert-file=/etc/kubernetes/ssl/ca.pem \\
+--cluster-signing-key-file=/etc/kubernetes/ssl/ca-key.pem \\
+--service-account-private-key-file=/etc/kubernetes/ssl/ca-key.pem \\
+--root-ca-file=/etc/kubernetes/ssl/ca.pem \\
+--leader-elect=true \\
+--etcd-servers=http://${MASTER_NAME}:2379 \\
+--logtostderr=true \\
+--v=0 \\
+--allow-privileged=true
+--master=http://${MASTER_NAME}:8080
+Restart=on-failure
+RestartSec=5
+[Install]
+WantedBy=multi-user.target
+EOF
+
+	#创建scheduler.service
+	cat > kube-controller-manager.service <<EOF
+[Unit]
+Description=Kubernetes Scheduler
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+[Service]
+ExecStart=/usr/bin/kube-scheduler \\
+--address=127.0.0.1 \\
+--leader-elect=true \\
+--etcd-servers=http://${MASTER_NAME}:2379 \\
+--logtostderr=true \\
+--v=0 \\
+--allow-privileged=true
+--master=http://${MASTER_NAME}:8080
+Restart=on-failure
+RestartSec=5
+[Install]
+WantedBy=multi-user.target
+EOF
+
 	#create service config file: apiserver,controller-manager,scheduler
 	cpServiceConfig kube-apiserver.service
+	check_ok
 	cpServiceConfig kube-controller-manager.service
+	check_ok
 	cpServiceConfig kube-scheduler.service
-	
+	check_ok
 }
 
-function startKubeService(){
+startKubeService(){
 	#start service : apiserver,controller-manager,scheduler
 	enableAndStarService kube-apiserver
 	enableAndStarService kube-controller-manager
 	enableAndStarService kube-scheduler
 }
 
-function cpServiceConfig(){
+cpServiceConfig(){
 	echo "step:------> create $1 config "
 	sleep 1
 	
-	if [  -f "$serviceDir/$1" ]; then
+	if [  -f "${serviceDir}/$1" ]; then
 		echo "step:------>backup service config file "$1
         	sleep 1
-		mv $serviceDir/$1 $serviceDir/$1.bak
+		mv ${serviceDir}/$1 ${serviceDir}/$1.bak
 		check_ok
     fi
 		
-    cp $baseDir/master/repo/$1 $serviceDir/
+    cp ${baseDir}/master/$1 ${serviceDir}/
 	check_ok
 	echo "step:------> create $1 config completed."
 	sleep 1
 }
 
-function installEtcdAndFlannel(){
+installEtcdAndFlannel(){
 	echo "step:------> install etcd flannel package"
 	rpm -e --nodeps etcd
 	rpm -e --nodeps flannel
@@ -172,42 +248,158 @@ function installEtcdAndFlannel(){
 	echo "step:------> install etcd flannel package completed."
 }
 
-function configEtcd(){
-	echo "step:------> config etcd"
+configEtcd(){
+	echo "step:------> config etcd "
 	sleep 1
-	mv /etc/etcd/etcd.conf /etc/etcd/etcd.conf.bak
-	sed -e "s/localhost/0.0.0.0/g" /etc/etcd/etcd.conf.bak > /etc/etcd/etcd.conf
+	
+	if [ ! -f "${baseDir}/master/etcd/${etcd_file}" ]; then
+	        wget https://github.com/coreos/etcd/releases/download/${etcd_version}/${etcd_file}
+			check_ok
+	fi
+	tar -zxf ${baseDir}/master/etcd/${etcd_file}
+	mv ${baseDir}/master/etcd/etcd-v3.1.9-linux-amd64/etcd* /usr/bin/
+	chown root.root /usr/bin/etc*
+	rm -rf ${baseDir}/master/etcd/etcd-v3.1.9-linux-amd64
+	
+	cat > ${baseDir}/master/etcd/etcd.service <<EOF
+[Unit]
+Description=Etcd Server
+After=network.target
+After=network-online.target
+Wants=network-online.target
+Documentation=https://github.com/coreos
+[Service]
+Type=notify
+WorkingDirectory=/var/lib/etcd/
+EnvironmentFile=-/etc/etcd/etcd.conf
+ExecStart=/usr/bin/etcd \\
+--name=${ETC_NAME} \\
+--cert-file=/etc/kubernetes/ssl/kubernetes.pem \\
+--key-file=/etc/kubernetes/ssl/kubernetes-key.pem \\
+--peer-cert-file=/etc/kubernetes/ssl/kubernetes.pem \\
+--peer-key-file=/etc/kubernetes/ssl/kubernetes-key.pem \\
+--trusted-ca-file=/etc/kubernetes/ssl/ca.pem \\
+--peer-trusted-ca-file=/etc/kubernetes/ssl/ca.pem \\
+--initial-advertise-peer-urls=https://${MASTER_IP}:2380 \\
+--listen-peer-urls=https://${MASTER_IP}:2380 \\
+--listen-client-urls=https://${MASTER_IP}:2379,https://127.0.0.1:2379,http://127.0.0.1:2379 \\
+--advertise-client-urls=https://${MASTER_IP}:2379 \\
+--initial-cluster-token=etcd-cluster-0 \\
+--initial-cluster=${ETC_NAME}=https://${MASTER_NAME}:2380 \\
+--initial-cluster-state=new \\
+--data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65536
+[Install]
+WantedBy=multi-user.target
+EOF
+	
+	echo "step:------> config etcd completed."
+	sleep 1
+	
+	echo "step:------> ectd startup  "
+	sleep 1
+	rm -rf /lib/systemd/system/etcd.service
+	mv etcd.service /lib/systemd/system/
+	systemctl daemon-reload
+	systemctl enable etcd
+	systemctl restart etcd
 	check_ok
-	systemctl start etcd
+	
+	echo "step:------> ectd startup completed. "
+	sleep 1
+	
+	echo "step:------> check etcd "
+	sleep 1
+	etcdctl --ca-file=/etc/kubernetes/ssl/ca.pem --cert-file=/etc/kubernetes/ssl/kubernetes.pem --key-file=/etc/kubernetes/ssl/kubernetes-key.pem cluster-health
+	echo "step:------> check etcd completed"
+	sleep 1
+	
+	echo "step:------> config etcd network "
+	sleep 1
 	check_ok
 	etcdctl mkdir /kube-centos/network
 	etcdctl mk /kube-centos/network/config "{ \"Network\": \"172.30.0.0/16\", \"SubnetLen\": 24, \"Backend\": { \"Type\": \"vxlan\" } }"
-	echo "step:------> config etcd completed"
+	echo "step:------> config etcd network completed."
 	sleep 1	
 }
 
-function configFlannel(){
-	echo "step:------> config flannel"
+configFlannel(){
+	echo "step:------> config flannel "
 	sleep 1
-	mv /etc/sysconfig/flanneld /etc/sysconfig/flanneld.bak1
-	sed -e "s/127.0.0.1/$hostname/g" /etc/sysconfig/flanneld.bak1 > /etc/sysconfig/flanneld.bak2
-	sed -e "s/atomic.io/kube-centos/g" /etc/sysconfig/flanneld.bak2 > /etc/sysconfig/flanneld
+	
+	if [ ! -f "${baseDir}/master/flannel/${flannel_file}" ]; then
+	    wget https://github.com/coreos/flannel/releases/download/${flannel_version}/${flannel_file}
+		check_ok
+	fi
+	mkdir -p "${baseDir}/master/flannel/flannel
+	cd ${baseDir}/master/flannel
+	tar -zxf ${flannel_file} -C flannel 
+	cp flannel/{flanneld,mk-docker-opts.sh} /usr/bin
+	
+	cat > ${baseDir}/master/flannel/flanneld.service <<EOF
+[Unit]
+Description=Flanneld overlay address etcd agent
+After=network.target
+After=network-online.target
+Wants=network-online.target
+After=etcd.service
+Before=docker.service
+[Service]
+Type=notify
+ExecStart=/usr/bin/flanneld \\
+-etcd-cafile=/etc/kubernetes/ssl/ca.pem \\
+-etcd-certfile=/etc/kubernetes/ssl/kubernetes.pem \\
+-etcd-keyfile=/etc/kubernetes/ssl/kubernetes-key.pem \\
+-etcd-endpoints=https://${MASTER_NAME}:2379 \\
+-etcd-prefix=/kube-centos/network \\
+$FLANNEL_OPTIONS
+ExecStartPost=/root/local/bin/mk-docker-opts.sh -k DOCKER_NETWORK_OPTIONS -d /run/flannel/docker
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+RequiredBy=docker.service
+EOF
+	
+	echo "step:------> config etcd completed."
+	sleep 1
+	
+	echo "step:------> ectd startup  "
+	sleep 1
+	rm -rf /lib/systemd/system/etcd.service
+	mv etcd.service /lib/systemd/system/
+	systemctl daemon-reload
+	systemctl enable etcd
+	systemctl restart etcd
 	check_ok
-	systemctl start flanneld
+	
+	echo "step:------> ectd startup completed. "
+	sleep 1
+	
+	echo "step:------> check etcd "
+	sleep 1
+	etcdctl --ca-file=/etc/kubernetes/ssl/ca.pem --cert-file=/etc/kubernetes/ssl/kubernetes.pem --key-file=/etc/kubernetes/ssl/kubernetes-key.pem cluster-health
+	echo "step:------> check etcd completed"
+	sleep 1
+	
+	echo "step:------> config etcd network "
+	sleep 1
 	check_ok
-	echo "step:------> config flannel completed"
+	etcdctl mkdir /kube-centos/network
+	etcdctl mk /kube-centos/network/config "{ \"Network\": \"172.30.0.0/16\", \"SubnetLen\": 24, \"Backend\": { \"Type\": \"vxlan\" } }"
+	echo "step:------> config etcd network completed."
 	sleep 1	
 }
 
 closeSelinux
 closeIptables
-createK8scomponents
-
-createConfigFiles
-installEtcdAndFlannel
 configEtcd
+createK8scomponents
+createConfigFiles
+
+
 configFlannel
 startKubeService
 
 #echo "k8s-node installed complete!"
-
