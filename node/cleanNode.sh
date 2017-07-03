733ae63541154f155e56239a3be9a2842177ba94
suredandan <xrzp@qq.com>
2017-07-03 11:06:18 +0800

    modify cleanNode.sh

--- node/cleanNode.sh
+++ node/cleanNode.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 echo "***************begin to clean node's config ****************"
+echo "step:------> stop kubelet kube-proxy docker flanneld service "
+sleep 1
 systemctl stop kubelet kube-proxy docker flanneld
+echo "step:------> remove kubelet kube-proxy docker flannel config "
+sleep 1
 rm -rf /var/lib/kubelet
 rm -rf /var/lib/docker
 rm -rf /var/run/flannel/
 rm -rf /var/run/docker/
 rm -rf /usr/lib/systemd/system/{kubelet,docker,flanneld}.service
 rm -rf /usr/bin/{kubectl,kubelet,kube-proxy,docker*,flanneld}
+echo "step:------> remove kubernetes .kube .ssh directory"
+sleep 1
 rm -rf /etc/kubernetes
 rm -rf ~/.kube
+rm -rf ~/.ssh/
+echo "step:------> clean network config"
+sleep 1
 iptables -F && iptables -X && iptables -F -t nat && iptables -X -t nat
 ip link del flannel.1
 ip link del docker0
