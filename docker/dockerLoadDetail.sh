45ab2cc7c7b12d38a96c609a4da7ac101e6c0ea8
suredandan <xrzp@qq.com>
2017-07-11 16:49:21 +0800

    add other images

--- docker/dockerLoadDetail.sh
+++ docker/dockerLoadDetail.sh
@@ -5,4 +5,6 @@ docker load < dockerdir/k8s-dns-kube-dns-amd64.tar
 docker load < dockerdir/k8s-dns-sidecar-amd64.tar
 docker load < dockerdir/pause-amd64.tar
 docker load < dockerdir/pod-infrastructure.tar
+docker load < dockerdir/nginx_1.11.9.tar
+docker load < kubernetes-dashboard-amd64.tar
 docker images
